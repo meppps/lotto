98fdd74c81a4d0bdd9d439e8f5fe55f42bb4c8d0
Mikey Epps <55078570+meppps@users.noreply.github.com>
2021-02-02 14:15:17 -0800

    working, needs cleaned

--- index.js
+++ index.js
@@ -5,9 +5,25 @@ function randomNum(min,max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
+// 
+var numsToRoll = new Array(20).fill(0);
+var result = numsToRoll.map(x => x = randomNum(3,80));
+console.log(result);
+
+result.forEach((num)=>{
+  console.log(num);
+  var rand = document.createElement('h2');
+  rand.innerText = `${num}`;
+  rand.classList.add('Display');
+  rand.classList.add('frozen');
+  document.querySelector('.container').prepend(rand);
+  // document.querySelector('.capsule').prepend(rand);
+
+});
 
 // Roll down 
 function lotto(){
+
     var num = randomNum(3,50);
     var scrollFrom = num +  10;
     var currentNum;
@@ -16,21 +32,39 @@ function lotto(){
 
     const timer = ms => new Promise(res => setTimeout(res, ms))
     async function load () { // We need to wrap the loop into an async function for this to work
+        // currentNum = 34;
+        // document.querySelector('h2.Display').innerText = `${currentNum}`;
+        // document.querySelector('h2.bottomNum').style = `transform: none`;
 
-        var delay = 100;
-        var transform = 100;
-        for(i=1;i<11;i++) {
-        currentNum = scrollFrom - i;
-        console.log(currentNum);
-        document.querySelector('h2.Display').innerText = `${currentNum}`;
-        document.querySelector('h2.topNum').style.cssText = `transform: translateY(${transform}%) scale(.8);`;
-        document.querySelector('h2.bottomNum').style.cssText = `transform: translateY(${transform}%) scale(.8);`;
-          await timer(delay); // then the created Promise can be awaited
-          delay += 70;
-          transform += 10;
+        var delay = 10;
+        var transform = 30;
+        for(i=1;i<21;i++) {
+
+
+          console.log('yo')
+          // currentNum = scrollFrom - i;
+          // var newNum = document.createElement('h2');
+          // newNum.innerText = `${randomNum(3,50)}`;
+          // console.log(randomNum(3,50));
+          // document.querySelector('.container').prepend(newNum);
+          // newNum.classList.add('Display');
+          // newNum.classList.add('topNum');
+          
+          // document.querySelector('h2.Display').style = `transform: translateY(${transform}%) scale(.8);`;
+          document.querySelectorAll('h2.Display')[20-i].style = `transform: translateY(${transform}%) scale(.8);`;
+          // document.querySelector('div.capsule').style=`transform: translateY(${transform}%) scale(.8);`;
+          // newNum.classList = ['Display bottomNum'];
+          // document.querySelector('h2.bottomNum').remove();
+          // document.querySelector('h2.Display')
+            await timer(delay); // then the created Promise can be awaited
+            delay += 10;
+            // transform += 10;
+          
         }
       }
-
+    // for(i=0;i<5;i++){
+    //   load();
+    // }
     load();
 }
 
@@ -39,3 +73,23 @@ function lotto(){
 });
 
 
+async function roll(transform, delay){
+  
+
+
+  var newNum = document.createElement('h2');
+  newNum.classList.add('Display');
+  newNum.innerText = `${randomNum(3,50)}`;
+  document.querySelector('div.container').appendChild(newNum);
+  
+
+  for(i=0;i<10;i++){
+    newNum.style.cssText = `transform: translateY(${transform}%) scale(.8);`;
+    await timer(delay); // then the created Promise can be awaited
+    delay += 5;
+    transform += 10;
+  }
+}
+// roll();
+
+// document.querySelector('button').click()
