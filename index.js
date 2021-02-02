846d6df67c53d0a59adbe2a9c0788920a9f56df9
Mikey Epps <55078570+meppps@users.noreply.github.com>
2021-02-02 11:48:22 -0800

    Moving nums

--- index.js
+++ index.js
@@ -16,11 +16,18 @@ function lotto(){
 
     const timer = ms => new Promise(res => setTimeout(res, ms))
     async function load () { // We need to wrap the loop into an async function for this to work
+
+        var delay = 100;
+        var transform = 100;
         for(i=1;i<11;i++) {
         currentNum = scrollFrom - i;
         console.log(currentNum);
         document.querySelector('h2.Display').innerText = `${currentNum}`;
-          await timer(300); // then the created Promise can be awaited
+        document.querySelector('h2.topNum').style.cssText = `transform: translateY(${transform}%) scale(.8);`;
+        document.querySelector('h2.bottomNum').style.cssText = `transform: translateY(${transform}%) scale(.8);`;
+          await timer(delay); // then the created Promise can be awaited
+          delay += 70;
+          transform += 10;
         }
       }
 
@@ -30,3 +37,5 @@ function lotto(){
     document.querySelector('.getNum').addEventListener('click',()=>{
         lotto();
 });
+
+
